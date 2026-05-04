b0c6fa80f3b2e5b62f3d0dca63c7aaa865efe74b
Marcos Barros <barros.marcosf@gmail.com>
2026-05-03 21:45:44 -0300

    chore: auto-deploy 03/05 21:45

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1028,6 +1028,7 @@ let state = {
 
 
     function renderSection() {
+        document.documentElement.style.zoom = state.currentSection === 'dashboard' ? '0.90' : '1.08';
         // Destrói o DashboardChart adequadamente antes de remover o HTML
         if (dashboardChart) {
             dashboardChart.destroy();
